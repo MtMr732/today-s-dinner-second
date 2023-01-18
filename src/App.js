2464c0ab7873baf9dbb09b2737919ba0f1fa255f
toru_motomura\tbenc <tbencun156@gmail.com>
2023-01-18 18:41:03 +0900

    upload機能と取得機能実装

--- src/App.js
+++ src/App.js
@@ -1,36 +1,70 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import { gql, useQuery } from "@apollo/client";
 import Menus from "./components/Menus";
 
-const createMenu = gql`
-  mutation ($name: String!, $description: String!) {
-    createMenu(data: { name: $name, description: $description }) {
-      name
-      description
-    }
-  }
-`;
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  doc,
+  setDoc,
+  getDocs,
+  addDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
 
-function App() {
+const firebaseConfig = {
+  apiKey: "AIzaSyBYVxzP5thGHXYU_XnT2OnoVjs5h-bVCcI",
+  authDomain: "today-s-dinner-second.firebaseapp.com",
+  projectId: "today-s-dinner-second",
+  storageBucket: "today-s-dinner-second.appspot.com",
+  messagingSenderId: "1068876473605",
+  appId: "1:1068876473605:web:424273aa4b8e845627036c",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const storage = getStorage();
+
+const App = () => {
   const [todayMain, setTodayMain] = useState("ぎょうざ");
   const [todaySide, setTodaySide] = useState("サラダ");
   const [todayGarnish, settodayGarnish] = useState("みそしる");
-  const [isModalOpen, toggle] = useState(false);
+  const [isModalOpen, toggleModal] = useState(false);
+  const [file, setFile] = useState(null);
+  const [menus, setMenus] = useState(null);
 
-  const handlesubmit = () => {
-    createMenu();
+  // firestoreからデータを取得する
+  useEffect(() => {
+    const menusCollectionRef = collection(db, "menus");
+    getDocs(menusCollectionRef).then((querySnapshot) => {
+      // console.log(querySnapshot.docs);
+      setMenus(querySnapshot.docs);
+      // querySnapshot.docs.forEach((menu) => console.log(menu.data()));
+    });
+  }, []);
+
+  // inputタグにファイルが選択された際に発火するメソッド
+  const onChangeFile = (e) => {
+    setFile(e.target.files[0]);
+    console.log(e.target.files[0]);
   };
 
-  const displayMenu = () => {
-    // setTodayMain(data.length[Math.floor(Math.random() * data.length)].name);
-    // setTodaySide(data.length[Math.floor(Math.random() * data.length)].name);
-    // settodayGarnish(data.length[Math.floor(Math.random() * data.length)].name);
+  const onClickSubmit = async () => {
+    const storageRef = ref(storage, `images/${file.name}`);
+
+    // 画像のアップロード
+    await uploadBytes(storageRef, file).then((snapshot) => {
+      console.log("Uploaded a file", snapshot);
+    });
+
+    // dbへの保存(名前と内容と画像のパス)
   };
 
   const handleModal = () => {
-    toggle(!isModalOpen);
-    console.log(isModalOpen);
+    toggleModal(!isModalOpen);
   };
 
   useEffect(() => {
@@ -42,7 +76,7 @@ function App() {
       <h1 className='title'>today's dinner</h1>
 
       <div className='top'>
-        <button onClick={displayMenu}>献立を決める</button>
+        <button>献立を決める</button>
         <div className='wrapper'>
           <h3>今夜の晩ごはん</h3>
           <h3>メインディッシュ: {todayMain}</h3>
@@ -51,23 +85,35 @@ function App() {
         </div>
         <button onClick={handleModal}>メニューを追加する</button>
       </div>
-
       <div className='container'>
-        <Menus />
+        {menus?.map((menu) => (
+          <div key={menu.id}>
+            <div className='card'>
+              <img src={menu.data().imageURL}></img>
+              <p>{menu.data().name}</p>
+              <p>{menu.data().description}</p>
+            </div>
+          </div>
+        ))}
       </div>
 
       <div className={isModalOpen ? "modalOpen" : "modalFalse"}>
-        <div className='overlay'>
-          <form onSubmit={handlesubmit}>
-            <input placeholder='メニューの種別' />
-            <input placeholder='メニュー名' required />
-            <input placeholder='内容' multiple />
-            <input type='submit' />
-          </form>
+        <div className='overlay'></div>
+        <input placeholder='メニューの種別' />
+        <input placeholder='メニュー名' required />
+        <input placeholder='内容' multiple />
+        <input
+          type='file'
+          name='imageFile'
+          accept='image/*'
+          onChange={onChangeFile}
+        />
+        <div className='button'>
+          <button onClick={onClickSubmit} value='送信' />
         </div>
       </div>
     </>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,16 +4,9 @@ import "./index.css";
 import App from "./App";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-const client = new ApolloClient({
-  uri: "https://api-ap-northeast-1.hygraph.com/v2/clcsf0oi817xq01t9d6anfyxg/master",
-  cache: new InMemoryCache(),
-});
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ApolloProvider client={client}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ApolloProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
